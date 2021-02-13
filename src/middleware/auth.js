80529a3e15923f0332f510bd95841205b9b1ea55
Ankit Sodha <ankit.sodha123@gmail.com>
2021-02-13 17:18:40 +0530

    init commit

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,7 +4,7 @@ const User = require('../models/User');
 const auth = async (req, res, next) =>{
     //console.log("Authentication Middleware");
     const token=req.header('Authorization').replace('Bearer ','')
-    const decoded = jwt.verify(token, "SecretKey")
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
     const user=await User.findOne({_id : decoded._id, 'tokens.token':token})
     //console.log(user)
     if(!user){
